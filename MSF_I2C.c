8e2a497bb89f330ab44a49deca1e5be47d0b2c7e
Zachery Littell <zlittell@gmail.com>
2020-09-18 12:29:10 -0500

    add some various notes

--- MSF_I2C.c
+++ MSF_I2C.c
@@ -1,19 +1,16 @@
-/*
-  * MAIN FUNCTIONS FOR READ/WRITE
-  */
-unsigned char i2cReadByte(void)
-{
-
-}
-
-void i2cWriteByte(unsigned char toWrite)
-{
-
-}
-
-
-
-
-
-//I2C_INIT maybe should be in processor file
+//init
 //WaitForIdle
+//write byte
+//read byte
+//reset
+//start
+//repeated start
+//read with I2C_RESULT
+// stop
+//get_i2c_data_pointer
+//send_i2c_data
+//get_i2c_data_2byte
+//get_i2c_data_2byte_pointer
+
+//send data with length
+//receive data with length also allow pointers or have another function
