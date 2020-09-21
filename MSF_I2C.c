e4a1514558df75cfd8b2b0dab2cab0c3281e967c
Zachery Littell <zlittell@gmail.com>
2020-09-20 19:36:58 -0500

    set up i2c correctly. verified ~400KHz clockrate

--- MSF_I2C.c
+++ MSF_I2C.c
@@ -12,9 +12,9 @@ void init_i2c(void)
 	//IO lines
 	
 	//I2C_SDA - PA14 PAD0
-	//I2C_SCL - PA15 PAD1
-	PORT->Group[0].DIRSET.reg = PORT_PA14 | PORT_PA15;
 	PORT->Group[0].PMUX[7].reg = PORT_PMUX_PMUXE_C | PORT_PMUX_PMUXO_C;
+	PORT->Group[0].PINCFG[14].reg = PORT_PINCFG_PMUXEN;
+	PORT->Group[0].PINCFG[15].reg = PORT_PINCFG_PMUXEN;
 	
 	//PM
 	PM->APBCMASK.reg |= PM_APBCMASK_SERCOM0;
@@ -25,7 +25,7 @@ void init_i2c(void)
 		
 	SERCOM0->I2CM.CTRLB.reg = (SERCOM_I2CM_CTRLB_SMEN);
 	while(SERCOM0->I2CM.SYNCBUSY.reg);
-	SERCOM0->I2CM.BAUD.reg = 10;	//Should be 400KHz at 8MHz GCLK
+	SERCOM0->I2CM.BAUD.reg = 5;	//Should be 400KHz at 8MHz GCLK
 	while(SERCOM0->I2CM.SYNCBUSY.reg);
 	SERCOM0->I2CM.CTRLA.reg = (SERCOM_I2CM_CTRLA_SDAHOLD(3) | SERCOM_I2CM_CTRLA_MODE_I2C_MASTER | SERCOM_I2CM_CTRLA_ENABLE);
 	while(SERCOM0->I2CM.SYNCBUSY.reg);
@@ -38,6 +38,8 @@ bool i2c_start(uint8_t address)
 	SERCOM0->I2CM.ADDR.reg = ((address << 1) | 0);
 	
 	while(!(SERCOM0->I2CM.INTFLAG.reg & SERCOM_I2CM_INTFLAG_MB)); //wait for send
+	
+	SERCOM0->I2CM.INTFLAG.reg = SERCOM_I2CM_INTFLAG_MB;
 	
 	if (SERCOM0->I2CM.STATUS.reg & SERCOM_I2CM_STATUS_RXNACK)
 	{
@@ -54,6 +56,8 @@ bool i2c_write(uint8_t dataToSend)
 	SERCOM0->I2CM.DATA.reg = dataToSend;
 	
 	while(!(SERCOM0->I2CM.INTFLAG.reg & SERCOM_I2CM_INTFLAG_MB));	//wait for send
+	
+	SERCOM0->I2CM.INTFLAG.reg = SERCOM_I2CM_INTFLAG_MB;
 	
 	if(SERCOM0->I2CM.STATUS.reg & SERCOM_I2CM_STATUS_RXNACK)
 	{
